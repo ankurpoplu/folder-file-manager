6e3c994746d9041ed5b38757db8c3f5f44fe8096
ankur pandey <73213883+ankurpoplu@users.noreply.github.com>
2025-08-12 13:26:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use("/api/folders", folderRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8020;
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
